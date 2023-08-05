b551c2880f209788fb53001e144ae3c6fff4133c
remy.bernardin <remy.bernardin@cgi.com>
2023-08-05 14:33:57 +0200

    MYT-29 [AJOUT] - Création et connection de la BDD

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,30 @@
 
 const express = require('express');
 require('dotenv').config()
+const mysql = require('mysql2');
+
+// routes import
 const taskRoutes = require('./src/routes/task');
 const userRoutes = require('./src/routes/user');
 const badgeRoutes = require('./src/routes/badge');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution, 'DB connection success')
+})
+
+connection.end()
+
 
 // express app
 const app = express();
@@ -15,11 +36,10 @@ app.use((req, res, next) => {
     next()
 })
 
-
-//routes
 app.use( '/api/task', taskRoutes);
 app.use( '/api/user', userRoutes);
 app.use( '/api/badge', badgeRoutes);
+
 
 //listen for requests
 app.listen(process.env.PORT, () => {
