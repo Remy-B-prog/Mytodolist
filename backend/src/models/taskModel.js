face4e402fcdc471a3ecd0fcef84207b1be434d6
remy.bernardin <remy.bernardin@cgi.com>
2023-08-08 09:00:33 +0200

    MYT-6 [ADD] Add a model sql to had all user assigned task

---
+++ backend/src/models/taskModel.js
@@ -0,0 +1,13 @@
+const database = require('../services/database');
+
+const getUserAssignedTask = (userId) => {
+  return database.query(`
+  SELECT task.id, task.title, task.description,task.earned_point,
+  FROM task,
+  INNER JOIN assigned_task AS AT ON AT.task_id = task.id,
+  WHERE AT.user_id = ?;'
+  `
+  [userId]);
+};
+
+module.exports = getUserAssignedTask;
