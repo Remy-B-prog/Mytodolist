fae1d0cb14d9162551ada2aabcf1c456379d1575
remy.bernardin <remy.bernardin@cgi.com>
2023-08-08 09:33:26 +0200

    MYT-6 [ADD] Add user middleware to get user id on token

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -1,3 +1,12 @@
-const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
+
+const getUserIdOnToken = (token) => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.id;
+}
+
+module.exports = {
+    getUserIdOnToken,
+}
+
 
