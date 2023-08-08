ffd395205eac6fb7042f20db5ab2a051be74c738
remy.bernardin <remy.bernardin@cgi.com>
2023-08-08 10:40:19 +0200

    MYT-6 [UPDATE]  Add a function to convert header authorisation to Token

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-const getUserIdOnToken = (token) => {
+// Function to convert a full baerer token to a user id
+const getUserIdOnToken = (baererToken) => {
+    token = baererToken.replace('Bearer ', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return decoded.id;
+    return decoded.sub;
 }
 
 module.exports = {
