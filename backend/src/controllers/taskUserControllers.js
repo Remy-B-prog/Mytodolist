508df4e793f34f572bef605cf1c715f642fc8ab8
remy.bernardin <remy.bernardin@cgi.com>
2023-08-08 10:35:41 +0200

    MYT-6 [ADD] Add controller to gett all user assigned task

--- backend/src/controllers/taskUserControllers.js
+++ backend/src/controllers/taskUserControllers.js
@@ -1,1 +1,22 @@
 const database = require('../services/database');
+const getAllUserAssignedTask = require('../models/taskModel');
+const { getUserIdOnToken } = require('../middleware/userMiddleware');
+
+// Get all user assigned task
+const getUserAssignedTask = (req, res) => {
+    const token = req.header('Authorization');
+    const userId = getUserIdOnToken(token);
+    getAllUserAssignedTask(userId)
+        .then(([result]) => {
+            if (result.length > 1) {
+                res.status(200).json(result);
+            }else{
+                res.status(404).json({mssg: 'No task found'});
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({mssg: 'Internal server error'});
+        })
+}
+
+module.exports = getUserAssignedTask;
