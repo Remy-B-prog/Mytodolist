db5e8c64bb0f333098cff431fe9d06b47ea1d07b
remy.bernardin <remy.bernardin@cgi.com>
2023-08-12 11:49:53 +0200

    MYT-7 [ADD] Add controller to post a new assigned task

--- backend/src/controllers/taskUserControllers.js
+++ backend/src/controllers/taskUserControllers.js
@@ -1,5 +1,6 @@
 const database = require('../services/database');
 const getAllUserAssignedTask = require('../models/taskModel');
+const postAssigneTask = require('../models/taskModel');
 const { getUserIdOnToken } = require('../middleware/userMiddleware');
 
 // Get all user assigned task
@@ -10,13 +11,28 @@ const getUserAssignedTask = (req, res) => {
         .then(([result]) => {
             if (result.length > 1) {
                 res.status(200).json(result);
-            }else{
-                res.status(404).json({mssg: 'No task found'});
+            } else {
+                res.status(404).json({ mssg: 'No task found' });
             }
         })
         .catch((error) => {
-            res.status(500).json({mssg: 'Internal server error'});
+            res.status(500).json({ mssg: 'Internal server error' });
         })
+}
+
+// Post a new task
+const postUserAssignedTask = (req, res) => {
+    const token = req.header('Authorization');
+    const userId = getUserIdOnToken(token);
+    const taskId = req.body.taskId;
+    postAssigneTask(userId, taskId)
+        .then(([result]) => {
+            if (result['affectedRows'] == 1) {
+                res.status(200).json({ mssg: 'Task assigned successfully' });
+            }else{
+                res.status(500).json({ mssg: 'Internal server error' });
+            }
+        });
 }
 
 module.exports = getUserAssignedTask;
