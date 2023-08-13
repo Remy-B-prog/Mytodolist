dc89a2f1a7bee62462c77250f99ba116272f0c2b
remy.bernardin <remy.bernardin@cgi.com>
2023-08-13 11:16:03 +0200

    [CLEAN] Nettoyage de la route user

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -10,14 +10,5 @@ router.post('/login', getUserByEmailWithPasswordAndPassToNext,verifyPassword)
 router.post('/register', hashPassword, addUser)
 // router.get('/login', verifyPassword)
 
-// Delete a new user
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'delete a user'})
-})
-
-// Delete a new user
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'update a user'})
-})
 
 module.exports = router
