72425e5a180cadbe74e589a2a9ff9d5d2f9899b2
remy.bernardin <remy.bernardin@cgi.com>
2023-08-13 10:57:06 +0200

    [FIX] Fix const declaration

--- backend/src/middleware/userMiddleware.js
+++ backend/src/middleware/userMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // Function to convert a full baerer token to a user id
 const getUserIdOnToken = (baererToken) => {
-    token = baererToken.replace('Bearer ', '');
+    const token = baererToken.replace('Bearer ', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     return decoded.sub;
 }
