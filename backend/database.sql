b9c2218298a63453b62303ff23064eca9f8dc027
remy.bernardin <remy.bernardin@cgi.com>
2023-08-13 11:15:36 +0200

    [FIX] champ obligatoire dans la table user

--- backend/database.sql
+++ backend/database.sql
@@ -6,9 +6,9 @@ DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE user(  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    firstname VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    hashed_password VARCHAR(255)
+    firstname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL
 ) COMMENT '';
 
 INSERT INTO `user` VALUES(1,'rémy','fefe@gmail.com','$argon2id$v=19$m=65536,t=3,p=1$UfHvadttyf6lLjfnAFBhPQ$6kEukJg4lp8uy2jYa3fyVC9kCKqIClu18YfhkKt+ir8'),(2,'Maxime','Faure@gmail.com','$argon2id$v=19$m=65536,t=3,p=1$amnWVASflI+WCKYLxGJkig$H4bs+7fUd8HhJ9kVcOAmrwXkJonhqrK60t4rVKPweWo');
