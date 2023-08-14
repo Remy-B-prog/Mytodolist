767d23b95d1742d6fcdf46b52eb8d35b7909ed28
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 17:19:24 +0200

    MYT-38 [ADD] Add route to update user firstname

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getUserByEmailWithPasswordAndPassToNext, verifyPassword, hashPassword  } = require('../middleware/auth');
-const  addUser  = require('../controllers/userControllers');
+const {verifyToken} = require('../middleware/auth')
+const  {addUser, userUpdateHisFirstName}  = require('../controllers/userControllers');
 
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post('/login', getUserByEmailWithPasswordAndPassToNext,verifyPassword)
 // router.get('/login', verifyPassword)
 router.post('/register', hashPassword, addUser)
 
+// Protected routes
+router.use(verifyToken);
+
+router.patch('/update-firstname', userUpdateHisFirstName);
 
 module.exports = router
