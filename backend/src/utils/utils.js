8fdf416819fbf67f134800d445f238ec4f537ad5
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 14:46:31 +0200

    MYT-10 [ADD] Add a function to add score to user

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -3,16 +3,12 @@ const { getOneTaskById } = require('../models/taskModel');
 const { addUserScoreToCategory } = require('../models/scoreModel');
 
 const addUserScoreToCategoryUtils = (taskId, userId) => {
+    console.log("utils task id",taskId);
     getOneTaskById(taskId)
         .then(([result]) => {
-            console.log(scoreEarned);
-            const scoreEarned = result[0].score;
-            addUserScoreToCategory(scoreEarned, taskId, userId)
-                .then(([result]) => {
-                    if (result.affectedRows) {
-                        res.status(200).json({ mssg: ' scoreEarned was added to user Score' });
-                    }
-                })
+            const scoreEarned = result[0].earned_point;
+            const taskCategory = result[0].task_category_id;
+            addUserScoreToCategory(scoreEarned, taskCategory, userId)
         })
 }
 
