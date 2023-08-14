01ec615fbd064e695a296b913ea18638695ae7ce
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 15:22:06 +0200

    MYT- [ADD] Ajout d'un model pour avec tout les badge d'un user dans une category

--- backend/src/models/badgeModel.js
+++ backend/src/models/badgeModel.js
@@ -11,6 +11,17 @@ const getAllBadgeUser = (userId) => {
     )
 }
 
+const getAllBadgeUserInCategory = (userId, categoryId) => {
+    return database.query(`
+    SELECT b.title, b.critical_score, tc.category
+    FROM badge AS b
+    INNER JOIN user_badge AS ub ON b.id = ub.badge_id
+    INNER JOIN task_category AS tc ON b.task_category_id = tc.id
+    WHERE ub.user_id = ?
+    AND tc.id = ?`,
+    [userId, categoryId])
+}
+
 const getAllCategoryBadge = (categoryId) => {
     return database.query(
         `SELECT b.title, b.critical_score, tc.category
@@ -25,4 +36,5 @@ const getAllCategoryBadge = (categoryId) => {
 module.exports = {
     getAllBadgeUser,
     getAllCategoryBadge,
+    getAllBadgeUserInCategory,
 };
