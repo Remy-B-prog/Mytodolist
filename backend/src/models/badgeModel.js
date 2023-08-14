eb411d70fa132e11874feb76b4f52a6eacc2a0bd
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 08:58:51 +0200

    MYT-9 [FIX] correct the sql request

--- backend/src/models/badgeModel.js
+++ backend/src/models/badgeModel.js
@@ -2,9 +2,12 @@ const database = require('../services/database');
 
 const getAllBadgeUser = (userId) => {
     return database.query(
-        `SELECT * FROM user_badge 
+        `SELECT b.title, b.critical_score, tc.category
+        FROM user_badge
+        INNER JOIN badge as b ON badge_id = badge_id
+        INNER JOIN task_category as tc ON b.task_category_id = b.task_category_id
         WHERE user_id = ?`,
-        [user_id]
+        [userId]
     )
 }
 
