8f6bc8f9e288ef42b60f641ebb897b17da20dc6f
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 11:42:34 +0200

    MYT-10 [ADD] Add model to add score to tabel score

---
+++ backend/src/models/score.js
@@ -0,0 +1,14 @@
+const database = require('../services/database');
+
+
+const addUserScoreToCategory = (scoreToAdd, category, user) => {
+   ` UPDATE score 
+    set score = score + ?
+    WHERE task_category_id = ?
+    AND user_id = ?`,
+    [scoreToAdd, category, user]
+}
+
+module.exports = {
+
+}
