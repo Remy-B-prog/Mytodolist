dde64b16f560989d975f191ecddb69f18a0fb945
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 08:58:19 +0200

    [FIX] remove console.log on controller

--- backend/src/controllers/badgeControllers.js
+++ backend/src/controllers/badgeControllers.js
@@ -1,10 +1,12 @@
 const { getAllBadgeUser } = require('../models/badgeModel');
+const {getUserIdOnToken } = require('../middleware/userMiddleware');
 
 const getAllUserBadge = (req, res) => {
     const token = req.header('Authorization');
     const userId = getUserIdOnToken(token);
     getAllBadgeUser(userId)
         .then((result)=>{
+            console.log(result);
             if(result[0]){
                 res.status(200).send(result);
             }else{
