8ba249056812326c8fe9f49a92e410c93a0ac1c8
remy.bernardin <remy.bernardin@cgi.com>
2023-08-14 16:18:59 +0200

    MYT-10 [ADD] Add function to add earned badge from an user

--- backend/src/models/badgeModel.js
+++ backend/src/models/badgeModel.js
@@ -13,7 +13,7 @@ const getAllBadgeUser = (userId) => {
 
 const getAllBadgeUserInCategory = (userId, categoryId) => {
     return database.query(`
-    SELECT b.title, b.critical_score, tc.category
+    SELECT b.id, b.title, b.critical_score, tc.category 
     FROM badge AS b
     INNER JOIN user_badge AS ub ON b.id = ub.badge_id
     INNER JOIN task_category AS tc ON b.task_category_id = tc.id
@@ -24,7 +24,7 @@ const getAllBadgeUserInCategory = (userId, categoryId) => {
 
 const getAllCategoryBadge = (categoryId) => {
     return database.query(
-        `SELECT b.title, b.critical_score, tc.category
+        `SELECT b.id, b.title, b.critical_score, tc.category
          FROM badge AS b 
          INNER JOIN task_category AS tc ON b.task_category_id = tc.id
          WHERE tc.id = ?`,
@@ -32,9 +32,17 @@ const getAllCategoryBadge = (categoryId) => {
     )
 }
 
+const insertEarnedBadge = (badgeId, userId) => {
+    return database.query(`
+    INSERT INTO user_badge (badge_id, user_id)`
+    [badgeId, userId]);
+    
+}
+
 
 module.exports = {
     getAllBadgeUser,
     getAllCategoryBadge,
     getAllBadgeUserInCategory,
+    insertEarnedBadge,
 };
