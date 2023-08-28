4df0796cfe439a42fc68a113a420d35e2ee17474
remy.bernardin <remy.bernardin@cgi.com>
2023-08-28 12:21:09 +0200

    MYT-21 [ADD] Add controller to get all badge user dont validate

--- backend/src/controllers/badgeControllers.js
+++ backend/src/controllers/badgeControllers.js
@@ -1,18 +1,42 @@
-const { getAllBadgeUser } = require('../models/badgeModel');
-const {getUserIdOnToken } = require('../middleware/userMiddleware');
+const { getAllBadgeUser, getAllBadge } = require('../models/badgeModel');
+const { getUserIdOnToken } = require('../middleware/userMiddleware');
 
 const getAllUserBadge = (req, res) => {
     const token = req.header('Authorization');
     const userId = getUserIdOnToken(token);
     getAllBadgeUser(userId)
-        .then((result)=>{
-            if(result[0]){
+        .then((result) => {
+            if (result[0]) {
                 res.status(200).send(result[0]);
-            }else{
+            } else {
                 res.status(404).send("no badge found");
             }
         })
 };
+
+const getUserNotValidatedBadge = (req, res) => {
+    const token = req.header('Authorization');
+    const userId = getUserIdOnToken(token);
+    getAllBadge()
+        .then((result) => {
+            if (result[0]) {
+                const allBadge = result[0];
+                getAllBadgeUser(userId)
+                    .then((result) => {
+                        if (result[0]) {
+                            const userBadge = result[0];
+                            const userBadgeId = userBadge.map((badge) => badge.id);
+                            const notValidatedBadge = allBadge.filter((badge) => !userBadgeId.includes(badge.id));
+                            res.status(200).send(notValidatedBadge);
+                        } else {
+                            res.status(404).send("no badge found");
+                        }
+                    });
+            } else {
+                res.status(404).send("no badge found");
+            }
+        })
+}
 
 module.exports = {
     getAllUserBadge
